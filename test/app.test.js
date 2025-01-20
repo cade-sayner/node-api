fcf5936b6736778297e6261d00fb9f70d6e3dae0
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-01-20 14:42:35 +0200

    testing added again

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+import assert from 'assert'
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('it should return the string \"hello world\"', function () {
+      assert.equal("hello world", "hello world");
+    });
+  });
+});
